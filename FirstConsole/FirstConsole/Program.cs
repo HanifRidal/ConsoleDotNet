87f976d9ce7f4d6f804577fff8485cfdf763ce57
unknown <hanifridal17@gmail.com>
2024-04-09 04:47:51 +0700

    First Commit

---
+++ FirstConsole/FirstConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+
+        int x = 0;
+        double y = 3.14;
+        float z = 7.7f;
+        bool kondisi = false;
+        char karakter = 'a';
+        string nama = "asep hidayat";
+
+        Console.WriteLine("bilangan bulat = " + x );
+        Console.WriteLine("bilangan desimal = " + y);
+        Console.WriteLine("bilangan desimal = " + z);
+        Console.WriteLine("boolean (1/0) = " + kondisi);
+        Console.WriteLine("karakter = " + karakter);
+        Console.WriteLine("string = " + nama);
+
+
+
+        Console.ReadLine();
+    }
+}
